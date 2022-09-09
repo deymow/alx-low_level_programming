2428e48bf1e22f6c6fe349d4b7254aedae1981b9
demola <ademolayemi13@gmail.com>
2022-09-09 12:49:26 -0700

    Bettry comment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
+
+
+
 /**
- * main Entry point
- * puts function prints out string
  *
- * Return: Answer 0 (Success)
- */
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
 int main(void)
+
 {
 	puts("\"Programming is like building a multilingual puzzle");
+
+
 	return (0);
+
 }
