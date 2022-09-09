c8d006124d56822e131131bfc0db481a12f3989d
demola <ademolayemi13@gmail.com>
2022-09-09 12:45:06 -0700

    Betty style comment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
  * main Entry point
+ * puts function prints out string
  *
  * Return: Answer 0 (Success)
  */
 int main(void)
 {
-	/*puts function prints string*/
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
