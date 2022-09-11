13c88ef4d03224089b45299e53802656ffe6c54c
demola <ademolayemi13@gmail.com>
2022-09-11 14:06:32 -0700

    Code changes

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,8 +7,13 @@
 int main(void)
 {
 	int n;
+
 	for (n = 0; n < 10; n++)
-		putchar((n % 10), + '0' );
+	{
+		putchar((n % 10) + '0');
+		putchar(',');
+		putchar(' ');
+	}
 	putchar('\n');
 
 	return (0);
