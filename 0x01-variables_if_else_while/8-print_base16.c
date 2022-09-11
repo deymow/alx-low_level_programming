eb162dc379c8767e68dee3c4d6e52790b89e8c94
demola <ademolayemi13@gmail.com>
2022-09-11 14:23:44 -0700

    My last commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,11 +9,12 @@ int main(void)
 	int n;
 	char d;
 
-	for (n = '0'; n < '10'; n++)
+	for (n = 0; n < 10; n++)
 		putchar((n % 10) + '0');
 
 	for (d = 'a'; d <= 'f'; d++)
 		putchar(d);
+	putchar('\n');
 
 	return (0);
 }
