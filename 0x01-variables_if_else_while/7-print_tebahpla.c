45b9e792bb404f063680ea467ea967230d8c84af
demola <ademolayemi13@gmail.com>
2022-09-11 12:15:02 -0700

    print Hexadecimal

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	char d;
+
+	for (n = 0; n < 10; n++)
+		putchar(n);
+	for (d = 'A'; d <= 'F'; n++)
+		putchar(d);
+	return (0);
+}
