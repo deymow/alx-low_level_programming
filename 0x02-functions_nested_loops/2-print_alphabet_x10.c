4b5dfdc4a27a2a47e66fe94605df0f327167f322
demola <ademolayemi13@gmail.com>
2022-09-20 02:53:44 -0700

    Trailing whitespaces and declaration

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,9 +2,7 @@
 
 /**
  * print_alphabet - Check description
- *
  * Description: It prints the alphabet in lowercase fallowed by a new line 
- *
  * Return: Nothing.
  */
 
@@ -12,6 +10,7 @@ void print_alphabet_x10(void)
 {
 	int i;
 	char c;
+
 	for (i = 0; i < 10; i++)
 	{
 		for (c = 'a'; c <= 'z'; c++)
