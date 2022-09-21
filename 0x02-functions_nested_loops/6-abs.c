21829f59aceb7e34beac2c7b6d9a691d5df30084
demola <ademolayemi13@gmail.com>
2022-09-21 10:04:02 -0700

    Betty

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,12 +2,11 @@
  * File: 6-abs.c
  * Auth: Ademola E Adeyemi
  */
-
 #include "main.h"
-
 /**
  * _abs - Computes the absolute value of an integer.
  * @n: The integer to be computed.
+ *
  * Description: This function returns absolute value of a number
  * Return: Absolute value of number
  * */
